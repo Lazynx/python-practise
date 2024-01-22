a206b4212c57c5cffd6a3c28e5c4263b19ee088a
Lazynx <frichaplay@gmail.com>
2024-01-22 10:00:44 +0600

    changes

--- lab3/functions_1/ex_5.py
+++ lab3/functions_1/ex_5.py
@@ -1,12 +1,11 @@
-from itertools import permutations
-
-
-def permutation(string):
-    perms = permutations(string)
-
-    for i_perm in perms:
-        print("".join(i_perm))
+def generate_permutations(string, current=""):
+    if not string:
+        print(current)
+    else:
+        for i in range(len(string)):
+            remaining_chars = string[:i] + string[i + 1:]
+            generate_permutations(remaining_chars, current + string[i])
 
 
 if __name__ == "__main__":
-    permutation("test")
+    generate_permutations("test")
