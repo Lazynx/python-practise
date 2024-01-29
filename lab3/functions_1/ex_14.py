ea57d80444a316d90d7cfee2af9a002082f0be73
Lazynx <frichaplay@gmail.com>
2024-01-29 08:53:32 +0600

    some_changes

--- lab3/functions_1/ex_14.py
+++ lab3/functions_1/ex_14.py
@@ -1,8 +1,8 @@
 from ex_1 import gram_to_ounce
-from ex_5 import permutation
+from ex_5 import generate_permutations
 from ex_12 import histogram
 
 
 print(gram_to_ounce(15))
-permutation("laba")
+generate_permutations("laba")
 histogram([4, 9, 10, 5])
